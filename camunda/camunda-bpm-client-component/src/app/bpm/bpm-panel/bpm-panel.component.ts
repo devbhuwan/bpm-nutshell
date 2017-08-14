82b33b3bf260e7413ec93838b184434b1e678d9f
Bhuwan Prasad Upadhyay <comp.er.bhuwan@gmail.com>
2017-08-14 17:38:37 +0400

    camunda

---
+++ camunda/camunda-bpm-client-component/src/app/bpm/bpm-panel/bpm-panel.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'bpm-client-bpm-panel',
+  templateUrl: './bpm-panel.component.html',
+  styleUrls: ['./bpm-panel.component.css']
+})
+export class BpmPanelComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
